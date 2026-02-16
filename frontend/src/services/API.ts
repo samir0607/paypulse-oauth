96137ac03b63b6109ff79f8cbc487d03bc5c0855
samir0607 <samir.gupta987@gmail.com>
2026-02-16 17:02:48 +0530

    fix backend url

--- frontend/src/services/API.ts
+++ frontend/src/services/API.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const APIv1 = axios.create({
-  baseURL: import.meta.env.BASE_URL
+  baseURL: import.meta.env.VITE_BACKEND_BASE_URL
 })
 
 export const integrateOAuth = async (
